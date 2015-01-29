4da294a161c24dd8f4bca668c642668e94886dae
Ira <iralsob@gmail.com>
2015-01-29 18:16:37 +0300

    новые изменения

--- js/main.js
+++ js/main.js
@@ -15,7 +15,7 @@ $(document).ready(function() {
 	$('a[href*=#]').bind("click", function(e){
 		var anchor = $(this);
 		$('html, body').stop().animate({
-			scrollTop: $(anchor.attr('href')).offset().top
+			scrollTop: $(anchor.attr('href')).offset(150).top
 		}, 1000);
 		e.preventDefault();
 	});
