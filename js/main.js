c15d17b29d11ddbf0a473a643d4bfa0a8447af94
Ira <iralsob@gmail.com>
2015-01-29 18:48:15 +0300

    новые изменения

--- js/main.js
+++ js/main.js
@@ -14,8 +14,6 @@ $(document).ready(function() {
 
 	$('a[href*=#]').bind("click", function(e){
 		var anchor = $(this);
-		
-
 		$('html, body').stop().animate({
 			scrollTop: $(anchor.attr('href')).offset().top - 110
 		}, 1000);
